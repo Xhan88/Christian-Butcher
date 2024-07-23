bf56fdd6f8e39f1c1371c33ea66d1eb0a7f8aa88
Christian Martinez <dantedeat@hotmail.com>
2024-07-23 07:13:02 -0600

    23-07-24

--- components/Description/AboutMe.jsx
+++ components/Description/AboutMe.jsx
@@ -1,12 +1,21 @@
+import { useEffect } from 'react';
 import './AboutMe.css'
+import AOS from 'aos';
+
+
 const AboutMe = () => {
+
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <div className='description'>
       
-      <p>
+      <p data-aos="fade-up">
         <span style={{fontSize: '4rem', color: 'rgba(139, 17, 17, 2.5)'}}> Lorem impsun</span>   
         ipsum dolor sit amet consectetur, adipisicing elit. Veniam ex nihil voluptatem, recusandae est ab soluta labore assumenda deserunt quidem beatae ratione? Error sint labore tenetur suscipit sunt. Vitae, consequatur repudiandae. Molestias quam voluptatum possimus amet quaerat animi ab et id veniam sequi nihil, corporis, alias, modi exercitationem. Ab, voluptatum!</p>
-        <img src="./beef-butcher-chan.png" alt="chris butcher" />
+        <img data-aos="fade-up" src="./beef-butcher-chan.png" alt="chris butcher" />
     </div>
   )
 }
--- components/Description/ButcherDescription.jsx
+++ components/Description/ButcherDescription.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import './ButcherDescription.css'
+import React, { useEffect } from 'react';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import './ButcherDescription.css';
 
 const ButcherDescription = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <div className='description'>
-      <img src="./chrisButcher.png" alt="chris butcher" />
-      <p>
-        <span style={{fontSize: '4rem', color: 'rgba(139, 17, 17, 2.5)'}}> Lorem impsun</span>   
-        ipsum dolor sit amet consectetur, adipisicing elit. Veniam ex nihil voluptatem, recusandae est ab soluta labore assumenda deserunt quidem beatae ratione? Error sint labore tenetur suscipit sunt. Vitae, consequatur repudiandae. Molestias quam voluptatum possimus amet quaerat animi ab et id veniam sequi nihil, corporis, alias, modi exercitationem. Ab, voluptatum!</p>
-      
+      <img data-aos="fade-up" src="./chrisButcher.png" alt="Chris Butcher" />
+      <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam ex nihil voluptatem, recusandae est ab soluta labore assumenda deserunt quidem beatae ratione? Error sint labore tenetur suscipit sunt. Vitae, consequatur repudiandae. Molestias quam voluptatum possimus amet quaerat animi ab et id veniam sequi nihil, corporis, alias, modi exercitationem. Ab, voluptatum!</p>
     </div>
   )
 }
 
-export default ButcherDescription
+export default ButcherDescription;
