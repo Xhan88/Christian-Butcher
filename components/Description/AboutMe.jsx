b87409140267dc835bb59bb49f3869009ffa0fa5
Christian Martinez <dantedeat@hotmail.com>
2024-07-24 22:31:54 -0600

    24-07-24

--- components/Description/AboutMe.jsx
+++ components/Description/AboutMe.jsx
@@ -14,7 +14,8 @@ const AboutMe = () => {
       
       <p data-aos="fade-up">
         <span style={{fontSize: '4rem', color: 'rgba(139, 17, 17, 2.5)'}}> I am always </span>   
-        looking for new techniques and methods to improve my skills and stay up to date with market trends. My goal is to offer my clients excellent service, but also an unparalleled experience with each visit.</p>         <img data-aos="fade-up" src="./beef-butcher-chan.png" alt="chris butcher" />
+        looking for new techniques and methods to improve my skills and stay up to date with market trends. My goal is to offer my clients excellent service, but also an unparalleled experience with each visit.</p>  
+               <img data-aos="fade-up" src="./beef-butcher-chan.png" alt="chris butcher" />
     </div>
   )
 }
