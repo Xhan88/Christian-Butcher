da3bb3447f404f7c1ecf262ac49883a546c377e0
Christian Martinez <dantedeat@hotmail.com>
2024-07-27 20:29:13 -0600

    27-07-24

--- components/fistNavBar/FirstNavBar.jsx
+++ components/fistNavBar/FirstNavBar.jsx
@@ -1,50 +1,90 @@
-
-
 import EmailIcon from '@mui/icons-material/Email';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import { slide as Menu } from 'react-burger-menu';
 import "./FirstNavBar.css";
 import { useEffect, useState } from 'react';
 
 const FirstNavBar = () => {
- const [ isTransparent, setIsTransparent ] = useState(false);
- const handleScrool = () => {
-  if(window.scrollY > 0 ) {
-    setIsTransparent(true)
-  }else {
-    setIsTransparent(false)
-  }
-};
+  const [isTransparent, setIsTransparent] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 728);
 
-useEffect(() => {
-  window.addEventListener('scroll', handleScrool);
-  return () => {
-    window.removeEventListener('scroll', handleScrool)
-  }
-},[]);
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsTransparent(true);
+    } else {
+      setIsTransparent(false);
+    }
+  };
+
+  const handleResize = () => {
+    setIsSmallScreen(window.innerWidth <= 728);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-     <nav className={`nav-bar-first ${isTransparent ? 'transparent' : ''}`}>
-        <div className="left-section">
-          <p className="contact-item"> <LocalPhoneIcon className="icon" /> +52 378 142 6360</p>
-          <a className='contact-item email' href="mailto:christianmq888@gmail.com"> <EmailIcon className="icon" /> christianmq888@gmail.com</a>
-        </div>
-        <div className="right-section">
-          <div className="social-networks">
-                <a className='facebook' target='_blank' href='https://www.facebook.com/christian.martinezquezada'> 
-                  <FacebookIcon style={{color: 'white'}} />
-                </a>
-                <a target='_blank' href="https://www.instagram.com/dantedeat2024/">
-                  <InstagramIcon style={{color: 'white'}}/>
-                </a>
-                <a target='_blank' href="https://w.app/butcher">
-                  <WhatsAppIcon style={{color: 'white'}}/>
-                </a>         
+    <nav className={`nav-bar-first ${isTransparent ? 'transparent' : ''}`}>
+      {isSmallScreen ? (
+        <Menu right>
+          <div className="menu-content">
+            <div className="menu-item">
+              <LocalPhoneIcon className="icon" /> +52 378 142 6360
+            </div>
+            <div className="menu-item">
+              <EmailIcon className="icon" /> 
+              <a href="mailto:christianmq888@gmail.com" className="contact-link">
+                christianmq888@gmail.com
+              </a>
+            </div>
+            <div className="social-networks">
+              <a className='facebook' target='_blank' rel='noopener noreferrer' href='https://www.facebook.com/christian.martinezquezada'>
+                <FacebookIcon style={{ color: 'white' }} />
+              </a>
+              <a target='_blank' rel='noopener noreferrer' href="https://www.instagram.com/dantedeat2024/">
+                <InstagramIcon style={{ color: 'white' }} />
+              </a>
+              <a target='_blank' rel='noopener noreferrer' href="https://w.app/butcher">
+                <WhatsAppIcon style={{ color: 'white' }} />
+              </a>
+            </div>
           </div>
-        </div>    
-     </nav>
+        </Menu>
+      ) : (
+        <>
+          <div className="left-section">
+            <p className="contact-item">
+              <LocalPhoneIcon className="icon" /> +52 378 142 6360
+            </p>
+            <a className='contact-item email' href="mailto:christianmq888@gmail.com">
+              <EmailIcon className="icon" /> christianmq888@gmail.com
+            </a>
+          </div>
+          <div className="right-section">
+            <div className="social-networks">
+              <a className='facebook' target='_blank' rel='noopener noreferrer' href='https://www.facebook.com/christian.martinezquezada'>
+                <FacebookIcon style={{ color: 'white' }} />
+              </a>
+              <a target='_blank' rel='noopener noreferrer' href="https://www.instagram.com/dantedeat2024/">
+                <InstagramIcon style={{ color: 'white' }} />
+              </a>
+              <a target='_blank' rel='noopener noreferrer' href="https://w.app/butcher">
+                <WhatsAppIcon style={{ color: 'white' }} />
+              </a>
+            </div>
+          </div>
+        </>
+      )}
+    </nav>
   );
 }
 
