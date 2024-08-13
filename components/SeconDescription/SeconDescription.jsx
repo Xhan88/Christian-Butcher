5d21b38d5a9b8474b68cadc55f341f8bd032bae2
Christian Martinez <dantedeat@hotmail.com>
2024-08-12 23:19:04 -0600

    12-08-24

--- components/SeconDescription/SeconDescription.jsx
+++ components/SeconDescription/SeconDescription.jsx
@@ -3,7 +3,10 @@ const SeconDescription = () => {
   return (
     <div className="description" >
       <video autoPlay muted controls className="working1" src="./working1.mp4"></video>
-      <p>A series of techniques Ive learned throughout my life have helped me become a highly valuable worker. From sausage making, grinding, cutting, and preparing meats to advising customers on the best cuts for their planned dishes, my expertise is broad. I excel in quality control, inventory management, and training and supervising staff. These are just a few of the qualities that define me, along with a deep commitment to delivering the best service and ensuring customer satisfaction in every aspect of my work. </p>
+      <p>
+        <span style={{fontSize: '6rem', color: 'rgba(139, 17, 17, 2.5)'}}> " </span> A series of techniques I've learned throughout my life have helped me become a highly valuable worker. From sausage making, grinding, cutting, and preparing meats to advising customers on the best cuts for their planned dishes, my expertise is broad. I excel in quality control, inventory management, and training and supervising staff. These are just a few of the qualities that define me, along with a deep commitment to delivering the best service and ensuring customer satisfaction in every aspect of my work. 
+        
+        </p>
     </div>
   )
 }
