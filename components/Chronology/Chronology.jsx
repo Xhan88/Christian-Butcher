a983e4b7356e9b29a624c78f82b043efdf8ef308
Christian Martinez <dantedeat@hotmail.com>
2024-08-15 17:48:43 -0600

    15-08-24

--- components/Chronology/Chronology.jsx
+++ components/Chronology/Chronology.jsx
@@ -1,13 +1,22 @@
 
 const Chronology = () => {
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quasi magni deleniti facilis iste quod illo beatae nisi dignissimos voluptatum.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quasi magni deleniti facilis iste quod illo beatae nisi dignissimos voluptatum.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quasi magni deleniti facilis iste quod illo beatae nisi dignissimos voluptatum.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quasi magni deleniti facilis iste quod illo beatae nisi dignissimos voluptatum.</p>
+    <div className="description">
+      <video autoPlay muted controls className="working1" src="./working1.mp4"></video>
+      <p>In 2013, I began my apprenticeship in butchery alongside my father and mother in a small family business, where I worked for 3 years.
+          During that time, I acquired a range of basic skills such as deboning, working with dangerous machinery, and maintaining order.
+
+          Subsequently, I worked at a large butcher shop known in my region as "Carnicería El Cali," where I was employed for three years.
+          During that period, I honed the teachings of my father, gaining more speed in my work, improving my ability to work as part of a team, and developing skills to train new employees.
+
+          Currently, I work at Carnicería Matamoros, a family business where I was given the position of professional butcher.
+          Here, I am responsible for controlling raw materials, making cuts, and providing customer service.
+
+          One never stops learning, and over the years, I have realized that there are great talents in the world of butchery.
+      </p>
       
     </div>
+
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import AboutMe from '../components/Description/AboutMe.jsx'
 import BeefVideo from '../components/videoSection/BeefVideo.jsx'
 import Footer from '../components/footer/Footer.jsx'
 import SeconDescription from '../components/SeconDescription/SeconDescription.jsx'
+import Chronology from '../components/Chronology/Chronology.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
 
    <>
@@ -23,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AboutMe />
     <BeefVideo />
     <SeconDescription />
+    <Chronology />
     <Footer />
 
 </> 
